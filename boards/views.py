5f95d1e9fdd52a2bedce5e1c4ecdfe1ed45bd9eb
Mattijs de Gruijter <mattijsdeg@gmail.com>
2023-12-11 16:22:33 +0100

    Initial commit

---
+++ boards/views.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Board, Topic, Post
+
+# Create your views here.
+
+from django.http import HttpResponse
+
+def home(request):
+    
+    boards = Board.objects.all()
+    return render(request, 'home.html', {'boards': boards})
+
+def board_topics(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'topics.html', {'board': board})
+
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    
+    if request.method == 'POST':
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+        
+        user = User.objects.first()   # TODO: get the currently logged in user
+        
+        topic = Topic.objects.create(subject=subject, board=board, starter=user)
+        post = Post.objects.create(message=message, topic=topic, created_by=user)
+        
+        return redirect('board_topics', pk=board.pk) # TODO: redirect to the created topic page
+        
+    return render(request, 'new_topic.html', {'board': board})
